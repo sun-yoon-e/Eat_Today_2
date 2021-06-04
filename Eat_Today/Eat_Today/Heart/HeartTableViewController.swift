42cc0afcfcf984bcff767c8990ab9bd5c2fdf72f
서뉴니 <shinee252525@icloud.com>
2021-06-05 07:18:00 +0900

    찜 초기화

--- Eat_Today/Eat_Today/Heart/HeartTableViewController.swift
+++ Eat_Today/Eat_Today/Heart/HeartTableViewController.swift
@@ -63,12 +63,13 @@ class HeartTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         self.view.backgroundColor = UIColor(colorStruct: bg)
     }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.view.backgroundColor = UIColor(colorStruct: bg)
+        tableView.reloadData()
     }
 
     override func numberOfSections(in tableView: UITableView) -> Int {
@@ -95,5 +96,21 @@ class HeartTableViewController: UITableViewController {
         }
         
         return cell
+    }
+    
+    @IBAction func pressedTrash(_ sender: Any) {
+        clearView()
+    }
+    
+    func clearView() {
+        let alert = UIAlertController(title: title, message: "찜 목록을 초기화 하시겠어요?", preferredStyle: .alert)
+        let action2 = UIAlertAction(title: "No!", style: .destructive)
+        let action = UIAlertAction(title: "Yes!", style: .default, handler: { action in
+            self.hearts.removeAll()
+            self.tableView.reloadData()
+        })
+        alert.addAction(action)
+        alert.addAction(action2)
+        present(alert, animated: true, completion: nil)
     }
 }
--- Eat_Today/Eat_Today/OptionTableViewController.swift
+++ Eat_Today/Eat_Today/OptionTableViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 class OptionTableViewController: UITableViewController {
 
     var bg:Color = color
+    var c = HeartTableViewController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.view.backgroundColor = UIColor(colorStruct: bg)
+        
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         self.view.backgroundColor = UIColor(colorStruct: bg)
+    }
+    
+    @IBAction func pressedTrash(_ sender: Any) {
+        let alert = UIAlertController(title: title, message: "찜 목록을 초기화 하시겠어요?", preferredStyle: .alert)
+        let action2 = UIAlertAction(title: "No!", style: .destructive)
+        let action = UIAlertAction(title: "Yes!", style: .default, handler: { action in
+            self.c.hearts.removeAll()
+            self.c.tableView.reloadData()
+        })
+        alert.addAction(action)
+        alert.addAction(action2)
+        present(alert, animated: true, completion: nil)
     }
 }
